a000913fde7b08ba6244ce1af7e82aa210de8ce0
Madhav Rawal <112850999+madhavrawal29@users.noreply.github.com>
2024-04-03 20:04:23 +0530

    UseNavigate and Dynamic Routes

---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export default function ProductDetailPage() {
+  const params = useParams();
+
+  return (
+    <>
+      <h1>Product Details Page</h1>
+      <p>{params.productId}</p>
+    </>
+  );
+}
